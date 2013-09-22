d4537ca9aee1b0d0ebd9f5a7bef3776701a87ac9
Robert McLaughlin <robert@sparkk.us>
2013-09-22 15:02:02 -0400

    made ssl optional

--- door/db/connection.py
+++ door/db/connection.py
@@ -16,6 +16,9 @@ _key_dir = os.path.join(_path, "ssl")
 _local = threading.local()
 
 def _connect():
+    """
+        Connects to the database using the door.config information
+    """
     global _local
     
     if hasattr(_local, 'conn') and _local.conn:
@@ -31,20 +34,20 @@ def _connect():
     db    = section['database']
     host  = section['host']
     port  = section['port']
-
-    ssl_ca   = os.path.join(_key_dir, "ca-cert.pem")
-    ssl_cert = os.path.join(_key_dir, "client-cert.pem")
-    ssl_key  = os.path.join(_key_dir, "client-key.pem")
+    
+    ssl_options = {}
+    if section['ssl'].strip().lower() == 'true':
+        ssl_options['ssl_ca']   = os.path.join(_key_dir, "ca-cert.pem")
+        ssl_options['ssl_cert'] = os.path.join(_key_dir, "client-cert.pem")
+        ssl_options['ssl_key']  = os.path.join(_key_dir, "client-key.pem")
+        ssl_options['ssl_verify_cert'] = True
 
     _local.conn = connector.connect(user=user,
                               passwd=pass_,
                               db=db,
                               host=host,
                               port=port,
-                              ssl_ca   =ssl_ca,
-                              ssl_cert =ssl_cert,
-                              ssl_key  =ssl_key,
-                              ssl_verify_cert=True)
+                              **ssl_options)
 
 def get_conn():
     if not hasattr(_local, 'conn') or not _local.conn:
